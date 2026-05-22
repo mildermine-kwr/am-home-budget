24cfaf3085f775be796cec625d1cb7b1b290c4e5
mildermine-kwr <kewalin.charn@gmail.com>
2026-05-22 23:26:15 +0700

    Create supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,8 @@
+import { createClient }
+  from '@supabase/supabase-js'
+
+export const supabase =
+  createClient(
+    'https://uyxsonpxogxoqqnkykzn.supabase.co/rest/v1/',
+    'sb_publishable_royVseO0Ur2vD6p1zL5how_rDd2uO1-'
+  )

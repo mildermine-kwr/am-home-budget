2cd45455cab637896f880502495a8b96a36d7242
mildermine-kwr <kewalin.charn@gmail.com>
2026-06-10 09:49:18 +0700

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -5,4 +5,4 @@ export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_ANON_KEY
 )
 
-console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL)
+console.log("SUPABASE URL RAW =", JSON.stringify(import.meta.env.VITE_SUPABASE_URL))
